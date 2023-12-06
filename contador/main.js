03c20f7d3f201b0fcc4e9452f1a5638f82f5d1b3
Matheus Mesquita de Sousa <144474289+MatheusMesquitaS@users.noreply.github.com>
2023-12-06 09:52:26 -0300

    Add files via upload

---
+++ contador/main.js
@@ -0,0 +1,26 @@
+
+const value = document.getElementById("value");
+const plusButton = document.getElementById("plus");
+const minusButton = document.getElementById("minus");
+const resetButton = document.getElementById("reset");
+
+const updateValue = () => {
+    value.innerHTML = count;
+};
+
+let count = 0;
+
+plusButton.addEventListener("click", () => {
+    count += 1;
+    updateValue();
+});
+
+minusButton.addEventListener("click", () => {
+    count -= 1;
+    updateValue();
+});
+
+resetButton.addEventListener("click", () => {
+    count = 0;
+    updateValue();
+});
